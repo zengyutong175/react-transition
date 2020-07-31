a704dd4136b7c2c007d248a4f767f5692701e50f
zengyutong175 <874492733@qq.com>
2020-07-31 14:08:36 +0800

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,9 @@
+<Transition
+    style={{
+        padding: 100,
+    }}
+    transition="all 0.2s cubic-bezier(0.62, 0.38, 0.25, 1) 0s"
+    newItemStyle={{ opacity: 0 }}
+    list={[{key:1,value:1},{key:2,value:2}]}
+    item={(value) => <span className="box">{value.value}</span>}
+/>
